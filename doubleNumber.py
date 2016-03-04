19350f89001ccf8b7ee1365f5917f61eb480c939
Jack Wolfson <jwolfson2017@pingry.org>
2016-03-04 11:06:36 -0500

    New code for running the double number that allows the user to try again.

--- doubleNumber.py
+++ doubleNumber.py
@@ -1,17 +1,34 @@
-error = True
+#error = True
 
-while error == True:
-	number = input("What number should I double? ")
+#while error == True:
+	#number = input("What number should I double? ")
 	
-	try:
-		number = float(number)
+	#try:
+		#number = float(number)
 		# print("double that is {}.".format(number * 2))
 		# BUT try block is only for exception causes
-	except ValueError:
-		print("Sorry, that's not a number.")
-		error = True
+	#except ValueError:
+		#print("Sorry, that's not a number.")
+		#error = True
 		# exit()
 
+	#else:
+		#print("Double that is {}.".format(number * 2))
+		#error = False
+		
+
+number = input("What number should I double? ")
+
+converted = False
+
+while not converted:
+	try:
+		number = float(number)
+	
+	except ValueError:
+		number = input("Sorry, that's not a number. Try again. ")
+		
 	else:
+		converted = True
+		
 		print("Double that is {}.".format(number * 2))
-		error = False
